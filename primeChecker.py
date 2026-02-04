9e2e704333d932722a45cdc79fbb2338f79bef52
mr.Toli <anatolibiru888@gmail.com>
2026-02-04 16:11:50 +0300

    Add prime number checker function

---
+++ primeChecker.py
@@ -0,0 +1,13 @@
+def prime_check(x):
+    if x % 2 != 0 or x == 2:
+        if x < 10 and x % 2 == 0 or x % 3 == 0 or x % 5 == 0 or x % 7 == 0:
+            return f"{x} is a prime number"
+        elif x % 2 == 0 or x % 3 == 0 or x % 5 == 0 or x % 7 == 0:
+            return f"{x} is not prime number"
+        else:
+            return f"{x} is a prime number"
+    else:
+        return f"{x} is not prime number"
+
+
+print(prime_check(12))
