ff4d88027b659bee21edb00b18d5b2c4f11a0563
mr.Toli <anatolibiru888@gmail.com>
2026-02-06 01:17:39 +0300

    Fix prime number check condition and update test case

--- primeChecker.py
+++ primeChecker.py
@@ -1,6 +1,6 @@
 def prime_check(x):
     if x % 2 != 0 or x == 2:
-        if x < 10 and x % 2 == 0 or x % 3 == 0 or x % 5 == 0 or x % 7 == 0:
+        if x < 9 and (x % 2 == 0 or x % 3 == 0 or x % 5 == 0 or x % 7 == 0):
             return f"{x} is a prime number"
         elif x % 2 == 0 or x % 3 == 0 or x % 5 == 0 or x % 7 == 0:
             return f"{x} is not prime number"
@@ -10,4 +10,4 @@ def prime_check(x):
         return f"{x} is not prime number"
 
 
-print(prime_check(12))
+print(prime_check(9))
